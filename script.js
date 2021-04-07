49bd37f4f224c00f2677de993e1b7faa105aae63
ken <ken_037729@hotmail.com>
2021-04-07 20:27:24 +0800

    Done popular section

--- script.js
+++ script.js
@@ -3,21 +3,44 @@ const APIHIGHRATE = "https://api.themoviedb.org/3/discover/movie?certification_c
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-const main = document.getElementById('main');
+const all = document.getElementById('all');
+const popular = document.getElementById('popular');
+const highrate = document.getElementById('highRate');
+
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const title = document.getElementById('title');
 
-getMovie(APIPOPULAR);
+var barPopular = document.getElementById('bar_popular');
+var barHigh = document.getElementById('bar_high');
 
-async function getMovie(url){
-    const res = await fetch(url);
-    const resData = await res.json();
+getPopularMovie(APIPOPULAR);
+getHighMovie(APIHIGHRATE);
 
-    showMovie(resData.results);
+async function getPopularMovie(urlPopular){
+    const resPopular = await fetch(urlPopular);
+    const resPopularData = await resPopular.json();
+
+    showPopularMovie(resPopularData.results);
 }
-function showMovie(data){
-    main.innerHTML = "";
+async function getHighMovie(urlHighrate){
+    const resHigh = await fetch(urlHighrate);
+    const resHighData = await resHigh.json();
 
+    showHighMovie(resHighData.results);
+}
+async function getAllMovie(urlAll){
+    const all = await fetch(urlAll)
+    const allData = await all.json();
+
+    showAllMovie(allData.results);
+}
+function showAllMovie(data){
+    all.innerHTML = "";
+    popular.innerHTML = ""
+    highrate.innerHTML = ""
+    barPopular.style.display = "none"
+    barHigh.style.display = "none"
     data.forEach((movie)=>{
         const { poster_path, title, vote_average, overview } = movie;
 
@@ -34,16 +57,68 @@ function showMovie(data){
             ${overview}
         </div>
         `;
-        main.appendChild(div);
+        all.appendChild(div);
     })
-
 }
+function showPopularMovie(data){
+    popular.innerHTML = "";
+    var count = 0;
+    data.forEach((movie)=>{
+        if(count <= 3){
+            const { poster_path, title, vote_average, overview } = movie;
+
+            const div = document.createElement("div");
+            div.classList.add("movie");
+            div.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h4>Overview: </h4>
+                ${overview}
+            </div>
+            `;
+            popular.appendChild(div);
+            count++;
+        }
+    })
+}
+function showHighMovie(data){
+    highrate.innerHTML = "";
+    var count = 0;
+    data.forEach((movie)=>{
+        if(count <= 3){
+            const { poster_path, title, vote_average, overview } = movie;
+
+            const div = document.createElement("div");
+            div.classList.add("movie");
+            div.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h4>Overview: </h4>
+                ${overview}
+            </div>
+            `;
+            highrate.appendChild(div);
+            count++;
+        }
+    })
+}
+title.addEventListener('click', function(){
+    window.location.reload();
+})
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
     const searchResult = search.value;
     if(searchResult){
-        getMovie(SEARCHAPI + searchResult);
+        getAllMovie(SEARCHAPI + searchResult);
 
         searchResult = "";
     }
